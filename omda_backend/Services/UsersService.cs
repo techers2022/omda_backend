07bc7435f7275aa8a0e410d05e8d31524215a210
George Catalin Balota <george.balota@fortech.ro>
2022-08-07 16:30:28 +0300

    created a simple structure for the api

---
+++ omda_backend/Services/UsersService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using OMDA.Database;
+using OMDA.Models.Entities;
+
+namespace OMDA.Services;
+
+public class UsersService
+{
+    private readonly IMongoCollection<User> _usersCollection;
+
+    public UsersService(IOptions<DatabaseConnectionSettings> userStoreDatabaseSettings)
+    {
+        var mongoDatabase = DatabaseConnectionSettings.Connect(userStoreDatabaseSettings);
+
+        _usersCollection = mongoDatabase.GetCollection<User>("Users");
+    }
+
+    public async Task<List<User>> GetAsync() =>
+        await _usersCollection.Find(_ => true).ToListAsync();
+
+    public async Task<User?> GetAsync(string id) =>
+        await _usersCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(User newUser) =>
+        await _usersCollection.InsertOneAsync(newUser);
+
+    public async Task UpdateAsync(string id, User updatedUser) =>
+        await _usersCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
+
+    public async Task RemoveAsync(string id) =>
+        await _usersCollection.DeleteOneAsync(x => x.Id == id);
+}
