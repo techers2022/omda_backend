22f2a9535d7c2778004995dd8abdd78569fb1538
machiavelio <machiaveliogb@gmail.com>
2022-09-22 20:05:17 +0300

    -

---
+++ omda_backend/Models/Request/LoginUser.cs
@@ -0,0 +1,8 @@
+﻿namespace OMDA.Models.Request;
+
+public class LoginUser
+{
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+}
---
+++ omda_backend/Models/Request/RegisterUser.cs
@@ -0,0 +1,12 @@
+﻿namespace OMDA.Models.Request;
+
+public class CreateUser
+{
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+}
